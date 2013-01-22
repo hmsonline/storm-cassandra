64ea99777ce5f3a6712647a72897707f53f81479
unknown <acollautt@W7LAP02248.zoo.hmsonline.com>
2013-01-22 10:25:21 -0500

    added filter and emit empty on failure option

--- src/main/java/com/hmsonline/storm/cassandra/trident/TridentCassandraLookupFunction.java
+++ src/main/java/com/hmsonline/storm/cassandra/trident/TridentCassandraLookupFunction.java
@@ -17,6 +17,8 @@ import com.hmsonline.storm.cassandra.bolt.mapper.TridentColumnMapper;
 import com.hmsonline.storm.cassandra.bolt.mapper.TridentTupleMapper;
 import com.hmsonline.storm.cassandra.client.AstyanaxClient;
 import com.hmsonline.storm.cassandra.client.CassandraClient;
+import java.util.ArrayList;
+import storm.trident.operation.Filter;
 
 public class TridentCassandraLookupFunction<K, V> implements Function {
     private static final long serialVersionUID = 12132012L;
@@ -30,6 +32,10 @@ public class TridentCassandraLookupFunction<K, V> implements Function {
     private Class<V> columnValueClass;
     private String clientConfigKey;
 
+    private Filter tupleFilter = null;      // used to prevent processing for tuples that should be skipped by the lookup
+    private int numberOfOutputFields = 1;   // used to emit when the incoming tuple doesn't pass the filter check
+    private boolean emitEmptyOnFailure = false;
+
     public TridentCassandraLookupFunction(String clientConfigKey, TridentTupleMapper<K, V> tupleMapper,
             TridentColumnMapper<K, V> columnMapper, Class<K> columnNameClass, Class<V> columnValueClass) {
         this.columnNameClass = columnNameClass;
@@ -37,6 +43,22 @@ public class TridentCassandraLookupFunction<K, V> implements Function {
         this.columnsMapper = columnMapper;
         this.tupleMapper = tupleMapper;
         this.clientConfigKey = clientConfigKey;
+    }
+    
+    public TridentCassandraLookupFunction(String clientConfigKey, TridentTupleMapper<K, V> tupleMapper,
+            TridentColumnMapper<K, V> columnMapper, Class<K> columnNameClass, Class<V> columnValueClass, boolean emitEmptyOnFailure) {
+        this(clientConfigKey, tupleMapper, columnMapper, columnNameClass, columnValueClass);
+        this.emitEmptyOnFailure = emitEmptyOnFailure;
+    }
+    
+    public void setFilter(Filter filter) {
+        this.tupleFilter = filter;
+    }
+    public void setNumberOfOutputFields(int numberOfFields) {
+        this.numberOfOutputFields = numberOfFields;
+    }
+    public void setEmitEmptyOnFailure(boolean emitEmptyOnFailure) {
+        this.emitEmptyOnFailure = emitEmptyOnFailure;
     }
 
     @Override
@@ -57,6 +79,10 @@ public class TridentCassandraLookupFunction<K, V> implements Function {
     public void execute(TridentTuple input, TridentCollector collector) {
         String columnFamily = tupleMapper.mapToColumnFamily(input);
         String rowKey = tupleMapper.mapToRowKey(input);
+        if (tupleFilter != null && !tupleFilter.isKeep(input)) {
+            collector.emit(createEmptyValues());
+            return;
+        }
         Object start = tupleMapper.mapToStartKey(input);
         Object end = tupleMapper.mapToEndKey(input);
 
@@ -74,7 +100,22 @@ public class TridentCassandraLookupFunction<K, V> implements Function {
             }
 
         } catch (Exception e) {
+            if (this.emitEmptyOnFailure) {
+                LOG.info("Error processing tuple and will be emitting empty values.");
+                collector.emit(createEmptyValues());
+            }
             LOG.warn("Could not emit for row [" + rowKey + "] from Cassandra." + " :" + e.getMessage(), e);
         }
+    }
+    private Values createEmptyValues() {
+        //String[] emptyValues = new String[this.numberOfOutputFields];
+        ArrayList<Object> emptyValues = new ArrayList<Object>();
+        for (int evc=0;evc<this.numberOfOutputFields;evc++) {
+            emptyValues.add("");
+//            emptyValues[evc] = "";
+        }
+//        Values valuesToEmit = new Values((Object)emptyValues);
+        Values valuesToEmit = new Values(emptyValues.toArray());
+        return valuesToEmit;
     }
 }
