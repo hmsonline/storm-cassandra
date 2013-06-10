018e2b4b988b707554a96af118bdf6c6bbb8b7f1
P. Taylor Goetz <ptgoetz@gmail.com>
2013-06-10 12:36:18 -0400

    added ability to specify batch mutation max size

--- src/main/java/com/hmsonline/storm/cassandra/trident/CassandraState.java
+++ src/main/java/com/hmsonline/storm/cassandra/trident/CassandraState.java
@@ -16,12 +16,20 @@ import storm.trident.tuple.TridentTuple;
 
 public class CassandraState implements State {
 	private static final Logger LOG = LoggerFactory.getLogger(CassandraState.class);
+	public static final int DEFAULT_MAX_BATCH_SIZE = 3000;
 	@SuppressWarnings("rawtypes")
 	private AstyanaxClient client = null;
 	@SuppressWarnings("rawtypes")
 	private TridentTupleMapper mapper = null;
 	
+	private int maxBatchSize = 0;
+	
 	public CassandraState(AstyanaxClient<?, ?, ?> client, TridentTupleMapper<?, ?, ?> mapper){
+		this(client, mapper, DEFAULT_MAX_BATCH_SIZE);
+	}
+	
+	public CassandraState(AstyanaxClient<?, ?, ?> client, TridentTupleMapper<?, ?, ?> mapper, int maxBatchSize){
+		this.maxBatchSize = maxBatchSize;
 		this.client = client;
 		this.mapper = mapper;
 	}
@@ -39,10 +47,23 @@ public class CassandraState implements State {
 	@SuppressWarnings("unchecked")
 	public void update(List<TridentTuple> tuples, TridentCollector collector){
 		LOG.debug("updating with {} tuples", tuples.size());
+		
 		try {
-			this.client.writeTuples(tuples, this.mapper);
-			for(TridentTuple tuple : tuples){
-				collector.emit(tuple.getValues());
+			if(this.maxBatchSize > 0){
+				int size = tuples.size();
+				int batchCount = size / this.maxBatchSize;
+				int count = 0;
+				for (int i = 0;i < batchCount;i++){
+					this.client.writeTuples(tuples.subList(i * this.maxBatchSize, (i + 1) * this.maxBatchSize), this.mapper);
+					count++;
+				}
+				this.client.writeTuples(tuples.subList(count * this.maxBatchSize, size), this.mapper);
+	
+				for(TridentTuple tuple : tuples){
+					collector.emit(tuple.getValues());
+				}
+			} else {
+				this.client.writeTuples(tuples, this.mapper);
 			}
 
 		} catch (Exception e) {
