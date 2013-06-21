1894a2f4476dd78e7e61393e4f7a04effce9a8c8
P. Taylor Goetz <ptgoetz@gmail.com>
2013-06-21 09:30:13 -0400

    refactor CassandraState and add trident state query support.

--- src/main/java/com/hmsonline/storm/cassandra/trident/CassandraStateFactory.java
+++ src/main/java/com/hmsonline/storm/cassandra/trident/CassandraStateFactory.java
@@ -16,27 +16,25 @@ import storm.trident.state.StateFactory;
 
 public class CassandraStateFactory implements StateFactory {
 
-	private static final long serialVersionUID = 1055824326488179872L;
+    private static final long serialVersionUID = 1055824326488179872L;
 
-	private static final Logger LOG = LoggerFactory.getLogger(CassandraStateFactory.class);
-	
-	private TridentTupleMapper<?, ?, ?> mapper;
-	private String configKey;
-	
-	public CassandraStateFactory(String configKey, TridentTupleMapper<?,?,?> mapper){
-		this.mapper = mapper;
-		this.configKey = configKey;
-	}
+    private static final Logger LOG = LoggerFactory.getLogger(CassandraStateFactory.class);
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	@Override
-	public State makeState(Map conf, IMetricsContext metrics,
-			int partitionIndex, int numPartitions) {
-		LOG.debug("makeState partitionIndex:{} numPartitions:{}", partitionIndex, numPartitions);
-		AstyanaxClient client = new AstyanaxClient();
-		client.start((Map)conf.get(this.configKey));
-		int batchMaxSize = Utils.getInt(Utils.get(conf, StormCassandraConstants.CASSANDRA_BATCH_MAX_SIZE, CassandraState.DEFAULT_MAX_BATCH_SIZE));
-		return new CassandraState(client, this.mapper, batchMaxSize);
-	}
+    private String configKey;
+
+    public CassandraStateFactory(String configKey) {
+        this.configKey = configKey;
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    public State makeState(Map conf, IMetricsContext metrics, int partitionIndex, int numPartitions) {
+        LOG.debug("makeState partitionIndex:{} numPartitions:{}", partitionIndex, numPartitions);
+        AstyanaxClient client = new AstyanaxClient();
+        client.start((Map) conf.get(this.configKey));
+        int batchMaxSize = Utils.getInt(Utils.get(conf, StormCassandraConstants.CASSANDRA_BATCH_MAX_SIZE,
+                CassandraState.DEFAULT_MAX_BATCH_SIZE));
+        return new CassandraState(client, batchMaxSize);
+    }
 
 }
--- src/main/java/com/hmsonline/storm/cassandra/trident/CassandraUpdater.java
+++ src/main/java/com/hmsonline/storm/cassandra/trident/CassandraUpdater.java
@@ -2,18 +2,25 @@ package com.hmsonline.storm.cassandra.trident;
 
 import java.util.List;
 
+import com.hmsonline.storm.cassandra.bolt.mapper.TridentTupleMapper;
+
 import storm.trident.operation.TridentCollector;
 import storm.trident.state.BaseStateUpdater;
 import storm.trident.tuple.TridentTuple;
 
-public class CassandraUpdater extends BaseStateUpdater<CassandraState> {
+public class CassandraUpdater<K, C, V> extends BaseStateUpdater<CassandraState> {
 
-	private static final long serialVersionUID = 1115563296010140546L;
+    private static final long serialVersionUID = 1115563296010140546L;
+    
+    private TridentTupleMapper<K, C, V> tupleMapper;
+    
+    public CassandraUpdater(TridentTupleMapper<K, C, V> tupleMapper){
+        this.tupleMapper = tupleMapper;
+    }
 
-	@Override
-	public void updateState(CassandraState state, List<TridentTuple> tuples,
-			TridentCollector collector) {
-		state.update(tuples, collector);
-	}
+    @Override
+    public void updateState(CassandraState state, List<TridentTuple> tuples, TridentCollector collector) {
+        state.update(tuples, collector, this.tupleMapper);
+    }
 
 }
