c9a0ba5b6dfae19e7a01c3c26356c5d86a914686
Homer Strong <homer.strong@gmail.com>
2013-10-25 10:49:15 -0700

    minor formatting and doc additions

--- src/main/java/com/hmsonline/storm/cassandra/bolt/mapper/CompositeRowTupleMapper.java
+++ src/main/java/com/hmsonline/storm/cassandra/bolt/mapper/CompositeRowTupleMapper.java
@@ -1,8 +1,8 @@
 package com.hmsonline.storm.cassandra.bolt.mapper;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Arrays;
 
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
@@ -20,6 +20,8 @@ public class CompositeRowTupleMapper implements TupleMapper<Composite, String, S
      * This mapper is similar to the DefaultTupleMapper, but supports composite row keys
      * constructed from multiple fields.
      *
+     * @param keyspace
+     *            keyspace to write to.
      * @param columnFamily
      *            column family to write to.
      * @param rowKeyFields
@@ -57,7 +59,7 @@ public class CompositeRowTupleMapper implements TupleMapper<Composite, String, S
      * in the Cassandra row, excluding fields that were included in the row.
      *
      * @param tuple
-     * @return
+     * @return map of columns to values
      */
     @Override
     public Map<String, String> mapToColumns(Tuple tuple) {
@@ -65,11 +67,11 @@ public class CompositeRowTupleMapper implements TupleMapper<Composite, String, S
         Map<String, String> columns = new HashMap<String, String>();
         for (int i = 0; i < fields.size(); i++) {
             String name = fields.get(i);
-	    Boolean isRowField = Arrays.asList(this.rowKeyFields).contains(name);
-	    if (!isRowField) {
-		Object value = tuple.getValueByField(name);
-		columns.put(name, (value != null ? value.toString() : ""));
-	    }
+            Boolean isRowField = Arrays.asList(this.rowKeyFields).contains(name);
+            if (!isRowField) {
+                Object value = tuple.getValueByField(name);
+                columns.put(name, (value != null ? value.toString() : ""));
+            }
         }
         return columns;
     }
